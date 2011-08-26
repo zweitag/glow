27b448ecb1b56cb7188d55d2d2f9d3264c7058af
Daniel Kirsch <daniel.kirsch@zweitag.de>
2011-08-26 11:39:40 +0200

    Add capybara

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,5 +6,7 @@ require "rails/test_help"
 
 Rails.backtrace_cleaner.remove_silencers!
 
+require 'capybara/rails'
+
 # Load support files
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
