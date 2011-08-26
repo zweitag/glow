e39385127ce563cc2f4585615feb6be2d2f1231f
Daniel Kirsch <daniel.kirsch@zweitag.de>
2011-08-26 13:26:18 +0200

    Fix functional test (only one request per test)

--- test/functional/flash_controller_test.rb
+++ test/functional/flash_controller_test.rb
@@ -1,18 +1,21 @@
 require 'test_helper'
 
+ActionDispatch::Flash::FlashHash.class_eval do
+  def discard?(key); @used.include?(key); end
+  def keep?(key); !discarded?(key); end
+end
+
 class FlashControllerTest < ActionController::TestCase
   def test_redirect
     post :create, type: :notice, message: 'Glow!'
     assert_equal flash[:notice], 'Glow!'
-    get :show # Flash survives
-    assert_equal flash[:notice], 'Glow!'
+    assert flash.keep?(:notice), 'Keep flash.'
   end
 
   def test_xhr
     xhr :post, :create, type: :notice, message: 'Glow!'
     assert_equal flash[:notice], 'Glow!'
     pending 'check headers'
-    get :show # Flash is discarded
-    assert flash.empty?
+    assert flash.discard?(:notice), 'Discard flash.'
   end
 end
