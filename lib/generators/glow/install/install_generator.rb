b32db7c518574144798b13c2f25e8554189661ed
Daniel Kirsch <daniel.kirsch@zweitag.de>
2011-08-30 09:08:48 +0200

    Move glow.coffee and add generator

---
+++ lib/generators/glow/install/install_generator.rb
@@ -0,0 +1,19 @@
+require 'rails'
+
+module Jquery
+  module Generators
+    class InstallGenerator < ::Rails::Generators::Base
+
+      desc "This generator installs glow.js. You must inclu"
+      class_option :ui, :type => :boolean, :default => false, :desc => "Include jQueryUI"
+      source_root File.expand_path('../../../../../vendor/assets/javascripts', __FILE__)
+
+      def copy_glow
+        say_status("copying", "glow", :green)
+        copy_file "glow.js", "public/javascripts/glow.js"
+        say_status("Please include glow.js in your layout", :green)
+      end
+
+    end
+  end
+end if ::Rails.version < "3.1"
