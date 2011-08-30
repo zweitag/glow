84d49364ad4fdc69af0b3bdd618dd7512d55ac7e
kaeff <kaeff@gmx.net>
2011-08-30 10:31:19 +0200

    Switched all tests to RSpec

--- test/dummy/spec/test_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe "the truth" do
-  it { should be == "the truth" }
-end
--- test/glow_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class GlowTest < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, Glow
-  end
-end
