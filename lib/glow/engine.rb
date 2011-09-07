8da046d11d90c4d9e9714e4338717d68c36dd347
Daniel Kirsch <daniel.kirsch@zweitag.de>
2011-09-07 15:22:28 +0200

    The Engine is now also called Engine

--- lib/glow/engine.rb
+++ lib/glow/engine.rb
@@ -2,7 +2,7 @@ require 'glow/filter'
 require 'action_controller/base'
 
 module Glow
-  class Railtie < Rails::Engine
+  class Engine < Rails::Engine
     config.to_prepare do
       ::ActionController::Base.send :include, Glow::Filter
     end
