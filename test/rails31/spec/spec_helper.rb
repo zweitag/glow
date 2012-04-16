ec227429887ae9c26be040b4386a663ec1c1dfc8
Daniel Kirsch <daniel.kirsch@zweitag.de>
2012-04-16 16:11:34 +0200

    test with capybara-webkit

--- test/rails31/spec/spec_helper.rb
+++ test/rails31/spec/spec_helper.rb
@@ -5,7 +5,7 @@ require 'rspec/rails'
 require 'capybara/rspec'
 require 'capybara/rails'
 
-Capybara.default_driver = :selenium
+Capybara.default_driver = :webkit
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
